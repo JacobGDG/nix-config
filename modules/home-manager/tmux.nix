45a3d35b31569c40873550a5e954c9452a2677e2
JacobGDG <10035081+JacobGDG@users.noreply.github.com>
2024-12-14 16:39:16 +0000

    Tmux theme and powerline

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -1,4 +1,25 @@
-{
+{ pkgs, lib, ... }:
+let
+  tmux-powerline = pkgs.tmuxPlugins.mkTmuxPlugin {
+    pluginName = "powerline";
+    version = "3.0.0";
+    src = pkgs.fetchFromGitHub {
+      owner = "erikw";
+      repo = "tmux-powerline";
+      rev = "2480e5531e0027e49a90eaf540f973e624443937";
+      hash = "sha256-25uG7OI8OHkdZ3GrTxG1ETNeDtW1K+sHu2DfJtVHVbk=";
+    };
+    rtpFilePath = "main.tmux";
+    meta = {
+      homepage = "https://github.com/erikw/tmux-powerline";
+      description = "Empowering your tmux (status bar) experience!";
+      longDescription = "A tmux plugin giving you a hackable status bar consisting of dynamic & beautiful looking powerline segments, written purely in bash.";
+      license = lib.licenses.bsd3;
+      platforms = lib.platforms.unix;
+      maintainers = with lib.maintainers; [ thomasjm ];
+    };
+  };
+in {
   imports = [
     ./sesh.nix
     ./tmuxifier.nix
@@ -14,6 +35,13 @@
     prefix = "C-a";
     escapeTime = 10;
     historyLimit = 50000;
+
+    # https://github.com/NixOS/nixpkgs/blob/master/pkgs/misc/tmux-plugins/default.nix
+    plugins = with pkgs; [
+      tmuxPlugins.gruvbox
+      tmux-powerline
+    ];
+
     extraConfig = ''
       #set -ag terminal-overrides ",xterm-256color:RGB"
       #set-option -sa terminal-features ',alacritty:RGB'
@@ -27,9 +55,9 @@
   
       # Remove Vim mode delays
       set -g focus-events on
-      set -g status-left-length 90
-      set -g status-right-length 90
-      set -g status-justify centre
+      # set -g status-left-length 90
+      # set -g status-right-length 90
+      # set -g status-justify centre
 
       set -g detach-on-destroy off  # don't exit from tmux when closing a session
 
