bd83cc4fa6f9b002018640e50b3de5fc1139a8ba
JacobGDG <10035081+JacobGDG@users.noreply.github.com>
2025-01-10 13:09:28 +0000

    Nvim update, lualine tweaks
    Remove unused code - might revisit this in future

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -8,12 +8,62 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     lazy = false,
-    depends = { 'nvim-tree/nvim-web-devicons' },
-    init = function()
-      require("lualine").setup {
-        extensions = { 'oil' },
-      }
-    end
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+      { "meuter/lualine-so-fancy.nvim", pin = true },
+    },
+    opts = {
+      options = {
+        icons_enabled = true,
+        theme = 'auto',
+        component_separators = { left = '', right = ''},
+        section_separators = { left = '', right = ''},
+        disabled_filetypes = {
+          statusline = {},
+          winbar = {},
+        },
+        ignore_focus = {},
+        always_divide_middle = true,
+        always_show_tabline = true,
+        globalstatus = false,
+        refresh = {
+          statusline = 100,
+          tabline = 100,
+          winbar = 100,
+        }
+      },
+      sections = {
+        lualine_a = {'fancy_mode'},
+        lualine_b = {'branch', 'diff', 'diagnostics'},
+        lualine_c = {
+          {
+            'filename',
+            path = 1,
+          },
+          'fancy_macro'
+        },
+        lualine_x = {'encoding', 'fileformat', 'filetype'},
+        lualine_y = {'location', 'progress'},
+        lualine_z = {'fancy_lsp_servers'}
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {
+          {
+            'filename',
+            path = 1,
+          }
+        },
+        lualine_x = {'location'},
+        lualine_y = {},
+        lualine_z = {}
+      },
+      tabline = {},
+      winbar = {},
+      inactive_winbar = {},
+      extensions = { 'oil' },
+    }
   },
 
   {
