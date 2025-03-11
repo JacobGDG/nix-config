eb0d47db479787468445aa41a8fdc4921691b3db
JacobGDG <10035081+JacobGDG@users.noreply.github.com>
2025-03-11 09:32:33 +0000

    Zen mode for nvim

--- config/nvim/lua/plugins/snacks.lua
+++ config/nvim/lua/plugins/snacks.lua
@@ -13,8 +13,21 @@ return {
       what = "file",
       branch = "HEAD"
     },
+    zen = {
+      enabled = true,
+      toggles = {
+        dim = true,
+      }
+    },
+    dim = {
+      animate = {
+        enabled = false,
+      },
+    },
   },
   keys = {
-      { '<leader>w', function() require("snacks").gitbrowse.open() end, mode = 'n', desc = "Open gitbrowse, ie open web", }
+    { '<leader>w', function() require("snacks").gitbrowse.open() end, mode = 'n', desc = "Open gitbrowse, ie open web", },
+    { "<leader>z",  function() require("snacks").zen() end, desc = "Toggle Zen Mode" },
+    { "<leader>Z",  function() require("snacks").zen.zoom() end, desc = "Toggle Zoom" },
   }
 }
