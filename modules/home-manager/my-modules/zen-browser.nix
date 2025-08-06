0ebea6d6807a9958692819939c1544aedbb85ffb
JacobGDG <10035081+JacobGDG@users.noreply.github.com>
2025-08-06 10:57:12 +0100

    feat: Experimenting with zen-browser settings
    My goal here is to have it setup enought that I dont need a Firefox
    account. So bookmarks, extentions, theming and more. Ideally I can
    easily have a work and personal profile that are very similar visually,
    but different bookmarks and logins. Maybe different themes to
    differenciate.
    
    As with the who repo, it is because I can, rather should 🤷

--- modules/home-manager/my-modules/zen-browser.nix
+++ modules/home-manager/my-modules/zen-browser.nix
@@ -52,6 +52,37 @@ in {
           };
         };
       };
+
+      profiles = {
+        personal = {
+          id = 0;
+          name = "personal";
+          isDefault = true;
+          settings = {
+            # "browser.startup.homepage" = "https://duckduckgo.com";
+            "browser.search.defaultenginename" = "ddg";
+            "browser.search.order.1" = "ddg";
+
+            "signon.rememberSignons" = false;
+            "widget.use-xdg-desktop-portal.file-picker" = 1;
+            "browser.aboutConfig.showWarning" = false;
+            "browser.compactmode.show" = true;
+            "browser.cache.disk.enable" = false; # Be kind to hard drive
+
+            "mousewheel.min_line_scroll_amount" = 50;
+
+            # Firefox 75+ remembers the last workspace it was opened on as part of its session management.
+            # This is annoying, because I can have a blank workspace, click Firefox from the launcher, and
+            # then have Firefox open on some other workspace.
+            "widget.disable-workspace-management" = true;
+          };
+          search = {
+            force = true;
+            default = "ddg";
+            order = ["ddg" "google"];
+          };
+        };
+      };
     };
   };
 }
