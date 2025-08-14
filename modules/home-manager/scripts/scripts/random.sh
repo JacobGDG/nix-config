41e09b2575f4269f3022efd57abf7e4b00ddcd71
JacobGDG <10035081+JacobGDG@users.noreply.github.com>
2025-08-14 16:01:19 +0100

    feat: add more character set options
    Maybe I should make this additive maybe, so -ans would add letters,
    numbers and symbols.
    
    A = uppercase
    a = lowercase
    n = numbers
    s = basic symbols
    S = special symbols
    
    maybe

--- modules/home-manager/scripts/scripts/random.sh
+++ modules/home-manager/scripts/scripts/random.sh
@@ -3,6 +3,8 @@
 # https://owasp.org/www-community/password-special-characters
 owaspCharset='A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_`{|}~'
 base64Charset="A-Za-z0-9+/="
+alphanumericCharset="A-Za-z0-9"
+numericCharset="0-9"
 
 function help() {
 cat << EOF >&2
@@ -13,8 +15,10 @@ Generate a random string of LENGTH characters.
 Options:"
   -h              Show this help message"
   -f (default)    Including the OWASP recommended characters"
-  -s              Use Simple base(Sixty)4 characters (A-Z, a-z, 0-9, +, /)"
+  -b              Use Base64 characters (A-Z, a-z, 0-9, +, /)"
   -v              Verbose output"
+  -a              Use only alphanumeric characters (A-Z, a-z, 0-9)"
+  -n              Use only number characters"
 EOF
 }
 
@@ -30,19 +34,38 @@ function error() {
 }
 
 verbose=0
+charset=""
 
 OPTIND=1
-while getopts hfsv opt; do
+while getopts hfbvan opt; do
     case $opt in
       h)
         help
         exit 0
         ;;
       f)
-        full=1
+        charset=$owaspCharset
         ;;
-      s)
-        simple=1
+      b)
+        if [[ -z $charset ]]; then
+          charset=$base64Charset
+        else
+          error "Cannot use multiple character sets at the same time."
+        fi
+        ;;
+      a)
+        if [[ -z $charset ]]; then
+          charset=$alphanumericCharset
+        else
+          error "Cannot use multiple character sets at the same time."
+        fi
+        ;;
+      n)
+        if [[ -z $charset ]]; then
+          charset=$numericCharset
+        else
+          error "Cannot use multiple character sets at the same time."
+        fi
         ;;
       v)
         verbose=1
@@ -63,13 +86,7 @@ if [[ $length -lt 1 ]]; then
   exit 1
 fi
 
-charset=""
-
-if [[ $full ]]; then
-  charset=$owaspCharset
-elif [[ $simple ]]; then
-  charset=$base64Charset
-else
+if [[ -z $charset ]]; then
   charset=$owaspCharset
 fi
 
@@ -82,7 +99,7 @@ entropy="$(password_entropy $password)"
 log "Password entropy: $entropy bits"
 
 if [[ $entropy -lt 80 ]]; then
-  echo "WARNING!!! Password entropy is low. Consider using a longer password or a different character set." 1>&2
+  echo "WARNING!!! Password entropy, $entropy bits, is low. Consider using a longer password or a different character set." 1>&2
 fi
 
 log
