b6da97cf1d6f0ad5c29032f3ee78351120f4290c
JacobGDG <10035081+JacobGDG@users.noreply.github.com>
2025-08-19 11:27:14 +0100

    refactor: set spell check in committia edit window

--- config/nvim/lua/plugins/git.lua
+++ config/nvim/lua/plugins/git.lua
@@ -15,44 +15,27 @@ return {
       vim.g.committia_edit_window_width = 80
     end,
     config = function()
+      local opts = {
+        buffer = vim.api.nvim_get_current_buf(),
+        silent = true,
+      }
+      local function map(mode, lhs, rhs)
+        vim.keymap.set(mode, lhs, rhs, opts)
+      end
+
       vim.g.committia_hooks = {
-        edit_open = function()
+        edit_open = function(info)
           vim.cmd.resize(10)
-          local opts = {
-            buffer = vim.api.nvim_get_current_buf(),
-            silent = true,
-          }
-          local function map(mode, lhs, rhs)
-            vim.keymap.set(mode, lhs, rhs, opts)
-          end
+
           map('n', 'q', '<cmd>quit<CR>')
           map('i', '<C-d>', '<Plug>(committia-scroll-diff-down-half)')
           map('n', '<C-d>', '<Plug>(committia-scroll-diff-down-half)')
           map('i', '<C-u>', '<Plug>(committia-scroll-diff-up-half)')
           map('n', '<C-u>', '<Plug>(committia-scroll-diff-up-half)')
           vim.cmd("highlight ColorColumn ctermbg=0 guibg=DarkGreen")
+          vim.cmd("setlocal spell")
           vim.api.nvim_set_option_value("colorcolumn", "51", {})
         end,
-        status_open = function()
-          local opts = {
-            buffer = vim.api.nvim_get_current_buf(),
-            silent = true,
-          }
-          local function map(mode, lhs, rhs)
-            vim.keymap.set(mode, lhs, rhs, opts)
-          end
-          map('n', 'q', '<cmd>quit<CR>')
-        end,
-        diff_open = function()
-          local opts = {
-            buffer = vim.api.nvim_get_current_buf(),
-            silent = true,
-          }
-          local function map(mode, lhs, rhs)
-            vim.keymap.set(mode, lhs, rhs, opts)
-          end
-          map('n', 'q', '<cmd>quit<CR>')
-        end
       }
     end,
   },
