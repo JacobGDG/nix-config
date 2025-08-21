8d432627cc0e588ced98b5b88a7079432c3096f8
JacobGDG <10035081+JacobGDG@users.noreply.github.com>
2025-08-21 13:51:44 +0100

    chore: remove Yanky, add hl autocommand

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,2 +1,17 @@
 require("set")
 require("lazy_init")
+
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 1000,
+        })
+    end,
+})
