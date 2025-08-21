12394a1808faa5fbe55a1f0260746a0570b395fc
JacobGDG <10035081+JacobGDG@users.noreply.github.com>
2025-08-21 14:05:38 +0100

    refactor: some autocmds for nvim
    lets try not highlighting all the matches. It does get annoying!

--- config/nvim/lua/set.lua
+++ config/nvim/lua/set.lua
@@ -2,7 +2,7 @@ vim.opt.showmatch  = true
 vim.opt.ignorecase = true
 vim.opt.smartcase  = true
 
-vim.opt.hlsearch = true
+vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
 vim.opt.tabstop = 2
@@ -18,15 +18,6 @@ vim.opt.hidden = true
 vim.opt.number = true
 vim.opt.cursorline = true
 vim.opt.colorcolumn = "+1"
-
-vim.api.nvim_create_autocmd({"BufEnter", "FocusGained", "InsertLeave"}, {
-    pattern = "*",
-    command = "set relativenumber",
-  })
-vim.api.nvim_create_autocmd({"BufLeave", "FocusLost", "InsertEnter"}, {
-    pattern = "*",
-    command = "set norelativenumber",
-  })
 
 vim.opt.showcmd = true
 vim.opt.wildmode = longest,list
