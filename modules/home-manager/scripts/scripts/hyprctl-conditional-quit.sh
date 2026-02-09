1f8ddc07c3de7f9f1ff2397a3a581b8341c6dfee
JacobGDG <10035081+JacobGDG@users.noreply.github.com>
2026-02-09 21:47:20 +0000

    feat: create app

--- modules/home-manager/scripts/scripts/hyprctl-conditional-quit.sh
+++ modules/home-manager/scripts/scripts/hyprctl-conditional-quit.sh
@@ -17,7 +17,7 @@ active_window=$(hyprctl activewindow -j | jq -r '.class')
 
 echo $active_window
 
-if [[ $active_window =~ ^(steam_app_[0-9]+|dwarfort)$ ]]; then
+if [[ $active_window =~ ^(steam_app_[0-9]+|dwarfort|Minecraft.+)$ ]]; then
   notify_canceled_exit
   exit 1
 fi
