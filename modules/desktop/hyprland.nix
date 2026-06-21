99d6b0ede2fe338fe763b0c8e07109be1432425a
JacobGDG <10035081+JacobGDG@users.noreply.github.com>
2026-06-21 17:54:35 +0100

    fix: allow hyprland to set uswm config
    Fixes popups about start-hyprland and setting XDG_CURRENT_DESKTOP

--- modules/desktop/hyprland.nix
+++ modules/desktop/hyprland.nix
@@ -10,15 +10,6 @@
     programs = {
       hyprlock.enable = true;
 
-      uwsm = {
-        enable = true;
-        waylandCompositors.hyprland = {
-          prettyName = "Hyprland";
-          comment = "Hyprland compositor managed by UWSM";
-          binPath = "/run/current-system/sw/bin/Hyprland";
-        };
-      };
-
       hyprland = {
         enable = true;
         withUWSM = true;
