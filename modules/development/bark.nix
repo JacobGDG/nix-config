16cfd349f2ec89f5e18b09c70db58bf94a4973a4
JacobGDG <10035081+JacobGDG@users.noreply.github.com>
2026-07-20 11:03:42 +0100

    fix: remove deprecated system call

--- modules/development/bark.nix
+++ modules/development/bark.nix
@@ -3,8 +3,12 @@
     url = "git+ssh://git@github.com/JacobGDG/bark.git?shallow=1";
     inputs.nixpkgs.follows = "nixpkgs";
   };
-  
-  flake.modules.homeManager.bark = { pkgs, inputs, ... }: {
-    home.packages = [ inputs.bark.packages."${pkgs.system}".default ];
+
+  flake.modules.homeManager.bark = {
+    pkgs,
+    inputs,
+    ...
+  }: {
+    home.packages = [inputs.bark.packages."${pkgs.stdenv.hostPlatform.system}".default];
   };
 }
