16bb92d5ebdc5ace6a2a54085cdb3a4db838f009
JacobGDG <10035081+JacobGDG@users.noreply.github.com>
2026-08-01 16:14:13 +0100

    feat: migrate FF to onepassword

--- modules/desktop/firefox.nix
+++ modules/desktop/firefox.nix
@@ -68,10 +68,6 @@
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
             installation_mode = "force_installed";
           };
-          "support@lastpass.com" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/lastpass-password-manager/latest.xpi";
-            installation_mode = "force_installed";
-          };
           "sponsorBlocker@ajay.app" = {
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/sponsorblock/latest.xpi";
             installation_mode = "force_installed";
@@ -82,6 +78,10 @@
           };
           "leechblockng@proginosko.com" = {
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/leechblock_ng/latest.xpi";
+            installation_mode = "force_installed";
+          };
+          "{d634138d-c276-4fc8-924b-40a0ea21d284}" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/1password-x-password-manager/latest.xpi";
             installation_mode = "force_installed";
           };
         };
